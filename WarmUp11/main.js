cb4414ad81cda0c19728a93cde1ca0199e77e783
SofianSaleh <sofiane.saleh11@gmail.com>
2019-10-30 10:07:14 +0100

    warmUp11

---
+++ WarmUp11/main.js
@@ -0,0 +1,8 @@
+$('#left').on('click',function(){
+	$('#leftLi').append('<li>' + $('#text').val()+'</li>')
+})
+
+$('#right').on('click',function(){
+	$('#rightLi').append('<li>' + $('#text').val()+'</li>')
+})
+
