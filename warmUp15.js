20515e1c00b7106c53d08d65943f64b4c9d4b2e6
SofianSaleh <sofiane.saleh11@gmail.com>
2019-11-05 09:21:33 +0100

    warmUp15

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,18 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+function reverseStr(str) {
+
+	var string = str.split(' ');
+	var reverse = '';
+
+	for(var i = string.length - 1; i >= 0 ; i--) {
+
+		reverse += string[i] + ' ';
+
+	}
+
+	return reverse;
+
+}
