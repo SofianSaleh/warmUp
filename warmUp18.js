e487fa89cad91ce88132e4b83b9618a99addc7c8
SofianSaleh <sofiane.saleh11@gmail.com>
2019-11-09 09:53:58 +0100

    warmUp

--- warmUp18.js
+++ warmUp18.js
@@ -10,3 +10,35 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+
+	function symbolToNum(string){
+		var x = string.split('')
+		var y = string.split('')
+		var str = '';
+		var count1 = 1;
+		var arr = [];
+		for (var k = 0; k < x.length; k++){
+			if(x[k] === x[k+1]){
+				arr.push(x[k])
+			}
+		}
+		console.log(arr)
+			str = str + 1
+			
+			
+		for(var i = 0; i < string.length; i++) {
+			console.log(x)
+			console.log(arr[i])
+		if(x[0] === arr[i]) {
+			count1++
+			str = str + count1
+			arr.push(x[i])
+			x.splice(i,1)
+			
+		}else if(x[i] !== arr[i]){
+			str= str + 1
+			x.splice(i,1)
+		}
+			
+	}return str;
+	}
